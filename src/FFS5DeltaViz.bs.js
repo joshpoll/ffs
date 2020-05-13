3099fabb6ed7a8b4d35550c0bc6d73e2f884c19c
Josh Pollock <joshpoll@cs.washington.edu>
2020-05-12 17:45:30 -0700

    add flow to FFS5DeltaViz

---
+++ src/FFS5DeltaViz.bs.js
@@ -0,0 +1,504 @@
+'use strict';
+
+var List = require("bs-platform/lib/js/list.js");
+var Curry = require("bs-platform/lib/js/curry.js");
+var React = require("react");
+var Pervasives = require("bs-platform/lib/js/pervasives.js");
+var Belt_MapString = require("bs-platform/lib/js/belt_MapString.js");
+var Theia$Sidewinder = require("sidewinder/src/Theia.bs.js");
+var Rectangle$Sidewinder = require("sidewinder/src/Rectangle.bs.js");
+
+function hSeq(uid, flow, $staropt$star, nodes) {
+  var gap = $staropt$star !== undefined ? $staropt$star : 0;
+  return Theia$Sidewinder.seq(uid, flow, undefined, nodes, undefined, gap, /* LeftRight */2, /* () */0);
+}
+
+function vSeq(uid, flow, $staropt$star, nodes) {
+  var gap = $staropt$star !== undefined ? $staropt$star : 0;
+  return Theia$Sidewinder.seq(uid, flow, undefined, nodes, undefined, gap, /* UpDown */0, /* () */0);
+}
+
+function value(uid, flow, name, node) {
+  return Theia$Sidewinder.box(uid, flow, /* :: */[
+              name,
+              /* [] */0
+            ], 5, 5, node, /* [] */0, /* () */0);
+}
+
+function cell(uid, flow, name, node) {
+  return Theia$Sidewinder.box(uid, flow, /* :: */[
+              name,
+              /* [] */0
+            ], 5, 5, node, /* [] */0, /* () */0);
+}
+
+function split(list, n) {
+  var _i = n;
+  var _acc = /* [] */0;
+  var _l = list;
+  while(true) {
+    var l = _l;
+    var acc = _acc;
+    var i = _i;
+    if (l) {
+      if (i === 0) {
+        return /* tuple */[
+                List.rev(acc),
+                l
+              ];
+      } else {
+        _l = l[1];
+        _acc = /* :: */[
+          l[0],
+          acc
+        ];
+        _i = i - 1 | 0;
+        continue ;
+      }
+    } else {
+      return /* tuple */[
+              List.rev(acc),
+              /* [] */0
+            ];
+    }
+  };
+}
+
+function insert(x, xs, i) {
+  var match = split(xs, i);
+  return Pervasives.$at(match[0], /* :: */[
+              x,
+              match[1]
+            ]);
+}
+
+function kont(nodeBuilder, nodes, holePos, hole) {
+  return Curry._1(nodeBuilder, insert(hole, nodes, holePos));
+}
+
+function zipper_aux(focus, konts) {
+  if (konts) {
+    return Curry._1(konts[0], zipper_aux(focus, konts[1]));
+  } else {
+    return focus;
+  }
+}
+
+function zipper(uid, flow, focus, konts) {
+  return Theia$Sidewinder.noop(uid, flow, undefined, zipper_aux(focus, konts), /* [] */0, /* () */0);
+}
+
+function paren(x) {
+  return hSeq(undefined, undefined, undefined, /* :: */[
+              Theia$Sidewinder.str(undefined, undefined, undefined, "(", /* () */0),
+              /* :: */[
+                x,
+                /* :: */[
+                  Theia$Sidewinder.str(undefined, undefined, undefined, ")", /* () */0),
+                  /* [] */0
+                ]
+              ]
+            ]);
+}
+
+function vizVid(flow, param) {
+  var x_uid = param[0];
+  return Theia$Sidewinder.str(x_uid, Belt_MapString.get(flow, x_uid), undefined, param[1], /* () */0);
+}
+
+function vizInt(flow, param) {
+  var n_uid = param[0];
+  return Theia$Sidewinder.str(n_uid, Belt_MapString.get(flow, n_uid), undefined, String(param[1]), /* () */0);
+}
+
+function envToList(param) {
+  var e = param[1];
+  if (e) {
+    return /* :: */[
+            e[0],
+            envToList(e[1])
+          ];
+  } else {
+    return /* [] */0;
+  }
+}
+
+function ctxtsToList(param) {
+  var c = param[1];
+  if (c) {
+    return /* :: */[
+            c[0],
+            ctxtsToList(c[1])
+          ];
+  } else {
+    return /* [] */0;
+  }
+}
+
+function vizAExp(flow, param) {
+  var ae = param[1];
+  var ae_uid = param[0];
+  switch (ae.tag | 0) {
+    case /* Var */0 :
+        return Theia$Sidewinder.noop(ae_uid, Belt_MapString.get(flow, ae_uid), undefined, vizVid(flow, ae[0]), /* [] */0, /* () */0);
+    case /* App */1 :
+        return hSeq(ae_uid, Belt_MapString.get(flow, ae_uid), 2, /* :: */[
+                    paren(vizAExp(flow, ae[0])),
+                    /* :: */[
+                      vizAExp(flow, ae[1]),
+                      /* [] */0
+                    ]
+                  ]);
+    case /* Lam */2 :
+        return Theia$Sidewinder.noop(ae_uid, Belt_MapString.get(flow, ae_uid), undefined, vizLambda(flow, ae[0]), /* [] */0, /* () */0);
+    case /* Num */3 :
+        return Theia$Sidewinder.noop(ae_uid, Belt_MapString.get(flow, ae_uid), undefined, vizInt(flow, ae[0]), /* [] */0, /* () */0);
+    case /* Add */4 :
+        return hSeq(ae_uid, Belt_MapString.get(flow, ae_uid), 2, /* :: */[
+                    paren(vizAExp(flow, ae[0])),
+                    /* :: */[
+                      Theia$Sidewinder.str(undefined, undefined, undefined, "+", /* () */0),
+                      /* :: */[
+                        paren(vizAExp(flow, ae[1])),
+                        /* [] */0
+                      ]
+                    ]
+                  ]);
+    case /* Bracket */5 :
+        return hSeq(ae_uid, Belt_MapString.get(flow, ae_uid), 2, /* :: */[
+                    Theia$Sidewinder.str(undefined, undefined, undefined, "{", /* () */0),
+                    /* :: */[
+                      vizExp(flow, ae[0]),
+                      /* :: */[
+                        Theia$Sidewinder.str(undefined, undefined, undefined, "}", /* () */0),
+                        /* [] */0
+                      ]
+                    ]
+                  ]);
+    
+  }
+}
+
+function vizExp(flow, param) {
+  var e = param[1];
+  var e_uid = param[0];
+  if (e.tag) {
+    return vSeq(e_uid, Belt_MapString.get(flow, e_uid), undefined, /* :: */[
+                hSeq(undefined, undefined, 2, /* :: */[
+                      Theia$Sidewinder.str(undefined, undefined, undefined, "let", /* () */0),
+                      /* :: */[
+                        vizVid(flow, e[0]),
+                        /* :: */[
+                          Theia$Sidewinder.str(undefined, undefined, undefined, "=", /* () */0),
+                          /* :: */[
+                            vizAExp(flow, e[1]),
+                            /* :: */[
+                              Theia$Sidewinder.str(undefined, undefined, undefined, "in", /* () */0),
+                              /* [] */0
+                            ]
+                          ]
+                        ]
+                      ]
+                    ]),
+                /* :: */[
+                  vizExp(flow, e[2]),
+                  /* [] */0
+                ]
+              ]);
+  } else {
+    return Theia$Sidewinder.noop(e_uid, Belt_MapString.get(flow, e_uid), undefined, vizAExp(flow, e[0]), /* [] */0, /* () */0);
+  }
+}
+
+function vizValue(flow, param) {
+  var v = param[1];
+  var v_uid = param[0];
+  if (v.tag) {
+    return value(v_uid, Belt_MapString.get(flow, v_uid), "closure", hSeq(undefined, undefined, undefined, List.map((function (n) {
+                          return Theia$Sidewinder.box(undefined, undefined, undefined, undefined, undefined, n, /* [] */0, /* () */0);
+                        }), /* :: */[
+                        vizLambda(flow, v[0]),
+                        /* :: */[
+                          vizEnv(flow, v[1]),
+                          /* [] */0
+                        ]
+                      ])));
+  } else {
+    return value(v_uid, Belt_MapString.get(flow, v_uid), "num", vizInt(flow, v[0]));
+  }
+}
+
+function vizEnv(flow, param) {
+  var e_uid = param[0];
+  return Theia$Sidewinder.table(e_uid, Belt_MapString.get(flow, e_uid), undefined, /* :: */[
+              /* :: */[
+                Theia$Sidewinder.str(undefined, undefined, undefined, "Id", /* () */0),
+                /* :: */[
+                  Theia$Sidewinder.str(undefined, undefined, undefined, "Val", /* () */0),
+                  /* [] */0
+                ]
+              ],
+              List.rev(List.map((function (param) {
+                          return /* :: */[
+                                  vizVid(flow, param.vid),
+                                  /* :: */[
+                                    vizValue(flow, param.value_uid),
+                                    /* [] */0
+                                  ]
+                                ];
+                        }), envToList(/* tuple */[
+                            e_uid,
+                            param[1]
+                          ])))
+            ], (function (source, target) {
+                return React.createElement("line", {
+                            stroke: "black",
+                            x1: ((Rectangle$Sidewinder.x2(source) + Rectangle$Sidewinder.x1(target)) / 2).toString(),
+                            x2: ((Rectangle$Sidewinder.x2(source) + Rectangle$Sidewinder.x1(target)) / 2).toString(),
+                            y1: ((Rectangle$Sidewinder.y1(source) + Rectangle$Sidewinder.y1(target)) / 2).toString(),
+                            y2: ((Rectangle$Sidewinder.y2(source) + Rectangle$Sidewinder.y2(target)) / 2).toString()
+                          });
+              }), (function (source, target) {
+                return React.createElement("line", {
+                            stroke: "black",
+                            x1: ((Rectangle$Sidewinder.x1(source) + Rectangle$Sidewinder.x1(target)) / 2).toString(),
+                            x2: ((Rectangle$Sidewinder.x2(source) + Rectangle$Sidewinder.x2(target)) / 2).toString(),
+                            y1: ((Rectangle$Sidewinder.y2(source) + Rectangle$Sidewinder.y1(target)) / 2).toString(),
+                            y2: ((Rectangle$Sidewinder.y2(source) + Rectangle$Sidewinder.y1(target)) / 2).toString()
+                          });
+              }), 0, 0, /* LeftRight */2, /* UpDown */0, /* () */0);
+}
+
+function vizLambda(flow, param) {
+  var uid = param.uid;
+  return hSeq(uid, Belt_MapString.get(flow, uid), undefined, /* :: */[
+              Theia$Sidewinder.str(undefined, undefined, undefined, "\\", /* () */0),
+              /* :: */[
+                vizVid(flow, param.vid),
+                /* :: */[
+                  Theia$Sidewinder.str(undefined, undefined, undefined, ".", /* () */0),
+                  /* :: */[
+                    vizExp(flow, param.exp_uid),
+                    /* [] */0
+                  ]
+                ]
+              ]
+            ]);
+}
+
+function vizCtxt(flow, param) {
+  var c = param[1];
+  var c_uid = param[0];
+  switch (c.tag | 0) {
+    case /* AppL */0 :
+        var partial_arg_000 = vizAExp(flow, c[1]);
+        var partial_arg = /* :: */[
+          partial_arg_000,
+          /* [] */0
+        ];
+        var partial_arg$1 = 2;
+        var partial_arg$2 = Belt_MapString.get(flow, c_uid);
+        var partial_arg$3 = c_uid;
+        var partial_arg$4 = function (param) {
+          return hSeq(partial_arg$3, partial_arg$2, partial_arg$1, param);
+        };
+        return (function (param) {
+            return kont(partial_arg$4, partial_arg, 0, param);
+          });
+    case /* AppR */1 :
+        var partial_arg_000$1 = vizValue(flow, c[0]);
+        var partial_arg$5 = /* :: */[
+          partial_arg_000$1,
+          /* [] */0
+        ];
+        var partial_arg$6 = 2;
+        var partial_arg$7 = Belt_MapString.get(flow, c_uid);
+        var partial_arg$8 = c_uid;
+        var partial_arg$9 = function (param) {
+          return hSeq(partial_arg$8, partial_arg$7, partial_arg$6, param);
+        };
+        return (function (param) {
+            return kont(partial_arg$9, partial_arg$5, 1, param);
+          });
+    case /* LetL */2 :
+        var e2 = c[2];
+        var x = c[0];
+        return (function (hole) {
+            return vSeq(c_uid, Belt_MapString.get(flow, c_uid), undefined, /* :: */[
+                        hSeq(undefined, undefined, 2, insert(hole, /* :: */[
+                                  Theia$Sidewinder.str(undefined, undefined, undefined, "let", /* () */0),
+                                  /* :: */[
+                                    vizVid(flow, x),
+                                    /* :: */[
+                                      Theia$Sidewinder.str(undefined, undefined, undefined, "=", /* () */0),
+                                      /* :: */[
+                                        Theia$Sidewinder.str(undefined, undefined, undefined, "in", /* () */0),
+                                        /* [] */0
+                                      ]
+                                    ]
+                                  ]
+                                ], 3)),
+                        /* :: */[
+                          vizExp(flow, e2),
+                          /* [] */0
+                        ]
+                      ]);
+          });
+    case /* AddL */3 :
+        var partial_arg_000$2 = Theia$Sidewinder.str(undefined, undefined, undefined, "+", /* () */0);
+        var partial_arg_001 = /* :: */[
+          paren(vizAExp(flow, c[1])),
+          /* [] */0
+        ];
+        var partial_arg$10 = /* :: */[
+          partial_arg_000$2,
+          partial_arg_001
+        ];
+        var partial_arg$11 = 2;
+        var partial_arg$12 = Belt_MapString.get(flow, c_uid);
+        var partial_arg$13 = c_uid;
+        var partial_arg$14 = function (param) {
+          return hSeq(partial_arg$13, partial_arg$12, partial_arg$11, param);
+        };
+        return (function (param) {
+            return kont(partial_arg$14, partial_arg$10, 0, param);
+          });
+    case /* AddR */4 :
+        var partial_arg_000$3 = paren(vizValue(flow, c[0]));
+        var partial_arg_001$1 = /* :: */[
+          Theia$Sidewinder.str(undefined, undefined, undefined, "+", /* () */0),
+          /* [] */0
+        ];
+        var partial_arg$15 = /* :: */[
+          partial_arg_000$3,
+          partial_arg_001$1
+        ];
+        var partial_arg$16 = 2;
+        var partial_arg$17 = Belt_MapString.get(flow, c_uid);
+        var partial_arg$18 = c_uid;
+        var partial_arg$19 = function (param) {
+          return hSeq(partial_arg$18, partial_arg$17, partial_arg$16, param);
+        };
+        return (function (param) {
+            return kont(partial_arg$19, partial_arg$15, 2, param);
+          });
+    
+  }
+}
+
+function vizCtxts(flow, cs) {
+  return List.map((function (param) {
+                return vizCtxt(flow, param);
+              }), ctxtsToList(cs));
+}
+
+var hole = Theia$Sidewinder.atom(undefined, undefined, undefined, /* [] */0, React.createElement("rect", {
+          height: "10",
+          width: "10",
+          fill: "red",
+          x: "5",
+          y: "5"
+        }), Rectangle$Sidewinder.fromPointSize(0, 0, 10, 10), /* () */0);
+
+function vizFocus(flow, param) {
+  var f = param[1];
+  var f_uid = param[0];
+  switch (f.tag | 0) {
+    case /* AExp */0 :
+        return Theia$Sidewinder.noop(f_uid, Belt_MapString.get(flow, f_uid), undefined, vizAExp(flow, f[0]), /* [] */0, /* () */0);
+    case /* Exp */1 :
+        return Theia$Sidewinder.noop(f_uid, Belt_MapString.get(flow, f_uid), undefined, vizExp(flow, f[0]), /* [] */0, /* () */0);
+    case /* Value */2 :
+        return Theia$Sidewinder.noop(f_uid, Belt_MapString.get(flow, f_uid), undefined, vizValue(flow, f[0]), /* [] */0, /* () */0);
+    
+  }
+}
+
+function vizFrame(flow, param) {
+  var uid = param.uid;
+  return vSeq(uid, Belt_MapString.get(flow, uid), undefined, /* :: */[
+              vizEnv(flow, param.env_uid),
+              /* :: */[
+                zipper(undefined, undefined, hole, vizCtxts(flow, param.ctxts_uid)),
+                /* [] */0
+              ]
+            ]);
+}
+
+function stackToList(param) {
+  var s = param[1];
+  if (s) {
+    return /* :: */[
+            s[0],
+            stackToList(s[1])
+          ];
+  } else {
+    return /* [] */0;
+  }
+}
+
+function vizStack(flow, param) {
+  var fs = param[1];
+  var s_uid = param[0];
+  if (fs) {
+    return vSeq(s_uid, Belt_MapString.get(flow, s_uid), undefined, List.map((function (param) {
+                      return vizFrame(flow, param);
+                    }), stackToList(/* tuple */[
+                        s_uid,
+                        fs
+                      ])));
+  } else {
+    return Theia$Sidewinder.str(s_uid, Belt_MapString.get(flow, s_uid), undefined, " ", /* () */0);
+  }
+}
+
+function vizMachineState(flow, param) {
+  var match = param.zipper;
+  var z_uid = match.uid;
+  var uid = param.uid;
+  return hSeq(uid, Belt_MapString.get(flow, uid), 20, /* :: */[
+              vSeq(undefined, undefined, 5, /* :: */[
+                    cell(undefined, undefined, "env", vizEnv(flow, param.env_uid)),
+                    /* :: */[
+                      zipper(z_uid, Belt_MapString.get(flow, z_uid), vizFocus(flow, match.focus_uid), vizCtxts(flow, match.ctxts_uid)),
+                      /* [] */0
+                    ]
+                  ]),
+              /* :: */[
+                cell(undefined, undefined, "stack", vizStack(flow, param.stack_uid)),
+                /* [] */0
+              ]
+            ]);
+}
+
+var MS = /* alias */0;
+
+exports.MS = MS;
+exports.hSeq = hSeq;
+exports.vSeq = vSeq;
+exports.value = value;
+exports.cell = cell;
+exports.split = split;
+exports.insert = insert;
+exports.kont = kont;
+exports.zipper_aux = zipper_aux;
+exports.zipper = zipper;
+exports.paren = paren;
+exports.vizVid = vizVid;
+exports.vizInt = vizInt;
+exports.envToList = envToList;
+exports.ctxtsToList = ctxtsToList;
+exports.vizAExp = vizAExp;
+exports.vizExp = vizExp;
+exports.vizValue = vizValue;
+exports.vizEnv = vizEnv;
+exports.vizLambda = vizLambda;
+exports.vizCtxt = vizCtxt;
+exports.vizCtxts = vizCtxts;
+exports.hole = hole;
+exports.vizFocus = vizFocus;
+exports.vizFrame = vizFrame;
+exports.stackToList = stackToList;
+exports.vizStack = vizStack;
+exports.vizMachineState = vizMachineState;
+/* hole Not a pure module */
---
+++ src/FFS5DeltaViz.re
@@ -0,0 +1,296 @@
+open FFS5Delta;
+open Sidewinder.Theia;
+
+/* TODO: maybe instead of a single uid, nodes can have multiple uids. but then they wouldn't be UIDs
+   but some other construct instead. unsure exactly why I might need this. */
+
+/* TODO: threading flow through every function, would be better e.g. refactor into a functor */
+
+module MS = Belt.Map.String;
+
+let hSeq = (~uid=?, ~flow=?, ~gap=0., nodes) =>
+  seq(~uid?, ~flow?, ~nodes, ~linkRender=None, ~gap, ~direction=LeftRight, ());
+let vSeq = (~uid=?, ~flow=?, ~gap=0., nodes) =>
+  seq(~uid?, ~flow?, ~nodes, ~linkRender=None, ~gap, ~direction=UpDown, ());
+let value = (~uid=?, ~flow=?, name, node) =>
+  box(~uid?, ~flow?, ~tags=[name], ~dx=5., ~dy=5., node, [], ());
+let cell = (~uid=?, ~flow=?, name, node) =>
+  box(~uid?, ~flow?, ~tags=[name], ~dx=5., ~dy=5., node, [], ());
+let split = (list, n) => {
+  let rec aux = (i, acc) =>
+    fun
+    | [] => (List.rev(acc), [])
+    | [h, ...t] as l =>
+      if (i == 0) {
+        (List.rev(acc), l);
+      } else {
+        aux(i - 1, [h, ...acc], t);
+      };
+  aux(n, [], list);
+};
+let insert = (x, xs, i) => {
+  let (xs, ys) = split(xs, i);
+  xs @ [x, ...ys];
+};
+
+let kont = (nodeBuilder, nodes, holePos, hole): Sidewinder.Kernel.node =>
+  nodeBuilder(insert(hole, nodes, holePos));
+
+let rec zipper_aux = (focus, konts) =>
+  switch (konts) {
+  | [] => focus
+  | [k, ...konts] => k(zipper_aux(focus, konts))
+  };
+
+let zipper = (~uid=?, ~flow=?, focus, konts) =>
+  noop(~uid?, ~flow?, zipper_aux(focus, konts), [], ());
+
+let paren = x => hSeq([str("(", ()), x, str(")", ())]);
+
+let vizVid = (flow, (x_uid, x)) => str(~uid=x_uid, ~flow=?MS.get(flow, x_uid), x, ());
+
+let vizInt = (flow, (n_uid, n)) =>
+  str(~uid=n_uid, ~flow=?MS.get(flow, n_uid), string_of_int(n), ());
+
+/* TODO: this strips away some of the values which are important to the visualization */
+let rec envToList = ((e_uid, e): env_uid): list(binding) =>
+  switch (e) {
+  | Empty => []
+  | Env(b, e) => [b, ...envToList(e)]
+  };
+
+/* TODO: this also strips things away */
+let rec ctxtsToList = ((c_uid, c): ctxts_uid): list(ctxt_uid) =>
+  switch (c) {
+  | Empty => []
+  | Ctxt(c, cs) => [c, ...ctxtsToList(cs)]
+  };
+
+let rec vizAExp = (flow, (ae_uid, ae)) =>
+  switch (ae) {
+  | Var(x) => noop(~uid=ae_uid, ~flow=?MS.get(flow, ae_uid), vizVid(flow, x), [], ())
+  | App(e1, e2) =>
+    hSeq(
+      ~uid=ae_uid,
+      ~flow=?MS.get(flow, ae_uid),
+      ~gap=2.,
+      [paren(vizAExp(flow, e1)), vizAExp(flow, e2)],
+    )
+  | Lam(lam) => noop(~uid=ae_uid, ~flow=?MS.get(flow, ae_uid), vizLambda(flow, lam), [], ())
+  | Num(n) => noop(~uid=ae_uid, ~flow=?MS.get(flow, ae_uid), vizInt(flow, n), [], ())
+  | Add(x, y) =>
+    hSeq(
+      ~uid=ae_uid,
+      ~flow=?MS.get(flow, ae_uid),
+      ~gap=2.,
+      [paren(vizAExp(flow, x)), str("+", ()), paren(vizAExp(flow, y))],
+    )
+  | Bracket(e) =>
+    hSeq(
+      ~uid=ae_uid,
+      ~flow=?MS.get(flow, ae_uid),
+      ~gap=2.,
+      [str("{", ()), vizExp(flow, e), str("}", ())],
+    )
+  }
+
+and vizExp = (flow, (e_uid, e)): Sidewinder.Kernel.node =>
+  switch (e) {
+  | Lift(ae) => noop(~uid=e_uid, ~flow=?MS.get(flow, e_uid), vizAExp(flow, ae), [], ())
+  | Let(x, ae1, e2) =>
+    vSeq(
+      ~uid=e_uid,
+      ~flow=?MS.get(flow, e_uid),
+      [
+        hSeq(
+          ~gap=2.,
+          [str("let", ()), vizVid(flow, x), str("=", ()), vizAExp(flow, ae1), str("in", ())],
+        ),
+        vizExp(flow, e2),
+      ],
+    )
+  }
+
+and vizValue = (flow, (v_uid, v)) =>
+  switch (v) {
+  | VNum(n) => value(~uid=v_uid, ~flow=?MS.get(flow, v_uid), "num", vizInt(flow, n))
+  | Clo(lam, e) =>
+    value(
+      ~uid=v_uid,
+      ~flow=?MS.get(flow, v_uid),
+      "closure",
+      hSeq(List.map(n => box(n, [], ()), [vizLambda(flow, lam), vizEnv(flow, e)])),
+    )
+  }
+
+and vizEnv = (flow, (e_uid, e)) =>
+  table(
+    ~uid=e_uid,
+    ~flow=?MS.get(flow, e_uid),
+    ~nodes=[
+      [str("Id", ()), str("Val", ())],
+      ...envToList((e_uid, e))
+         |> List.map(({vid, value_uid}) => [vizVid(flow, vid), vizValue(flow, value_uid)])
+         |> List.rev,
+    ],
+    ~xLinkRender=
+      Some(
+        (~source, ~target) =>
+          <line
+            x1={Js.Float.toString(
+              (source->Sidewinder.Rectangle.x2 +. target->Sidewinder.Rectangle.x1) /. 2.,
+            )}
+            x2={Js.Float.toString(
+              (source->Sidewinder.Rectangle.x2 +. target->Sidewinder.Rectangle.x1) /. 2.,
+            )}
+            y1={Js.Float.toString(
+              (source->Sidewinder.Rectangle.y1 +. target->Sidewinder.Rectangle.y1) /. 2.,
+            )}
+            y2={Js.Float.toString(
+              (source->Sidewinder.Rectangle.y2 +. target->Sidewinder.Rectangle.y2) /. 2.,
+            )}
+            stroke="black"
+          />,
+      ),
+    ~yLinkRender=
+      Some(
+        (~source, ~target) =>
+          <line
+            x1={Js.Float.toString(
+              (source->Sidewinder.Rectangle.x1 +. target->Sidewinder.Rectangle.x1) /. 2.,
+            )}
+            x2={Js.Float.toString(
+              (source->Sidewinder.Rectangle.x2 +. target->Sidewinder.Rectangle.x2) /. 2.,
+            )}
+            y1={Js.Float.toString(
+              (source->Sidewinder.Rectangle.y2 +. target->Sidewinder.Rectangle.y1) /. 2.,
+            )}
+            y2={Js.Float.toString(
+              (source->Sidewinder.Rectangle.y2 +. target->Sidewinder.Rectangle.y1) /. 2.,
+            )}
+            stroke="black"
+          />,
+      ),
+    ~xGap=0.,
+    ~yGap=0.,
+    ~xDirection=LeftRight,
+    ~yDirection=UpDown,
+    (),
+  )
+
+and vizLambda = (flow, {uid, vid, exp_uid}) =>
+  hSeq(
+    ~uid,
+    ~flow=?MS.get(flow, uid),
+    [str("\\", ()), vizVid(flow, vid), str(".", ()), vizExp(flow, exp_uid)],
+  );
+
+let vizCtxt = (flow, (c_uid, c)) =>
+  switch (c) {
+  /* TODO: add parens in the proper places */
+  | AppL((), ae) =>
+    kont(hSeq(~uid=c_uid, ~flow=?MS.get(flow, c_uid), ~gap=2.), [vizAExp(flow, ae)], 0)
+  | AppR(v, ()) =>
+    kont(hSeq(~uid=c_uid, ~flow=?MS.get(flow, c_uid), ~gap=2.), [vizValue(flow, v)], 1)
+  | LetL(x, (), e2) => (
+      hole => {
+        vSeq(
+          ~uid=c_uid,
+          ~flow=?MS.get(flow, c_uid),
+          [
+            hSeq(
+              ~gap=2.,
+              insert(
+                hole,
+                [str("let", ()), vizVid(flow, x), str("=", ()), str("in", ())],
+                3,
+              ),
+            ),
+            vizExp(flow, e2),
+          ],
+        );
+      }
+    )
+  /* TODO: add parens around hole */
+  | AddL((), ae) =>
+    kont(
+      hSeq(~uid=c_uid, ~flow=?MS.get(flow, c_uid), ~gap=2.),
+      [str("+", ()), paren(vizAExp(flow, ae))],
+      0,
+    )
+  | AddR(v, ()) =>
+    kont(
+      hSeq(~uid=c_uid, ~flow=?MS.get(flow, c_uid), ~gap=2.),
+      [paren(vizValue(flow, v)), str("+", ())],
+      2,
+    )
+  };
+
+let vizCtxts = (flow, cs) => ctxtsToList(cs) |> List.map(vizCtxt(flow));
+
+let hole =
+  atom(
+    ~links=[],
+    <rect fill="red" width="10" height="10" x="5" y="5" />,
+    Sidewinder.Rectangle.fromPointSize(~x=0., ~y=0., ~width=10., ~height=10.),
+    (),
+  );
+
+let vizFocus = (flow, (f_uid, f)) =>
+  switch (f) {
+  | AExp(ae) => noop(~uid=f_uid, ~flow=?MS.get(flow, f_uid), vizAExp(flow, ae), [], ())
+  | Exp(e) => noop(~uid=f_uid, ~flow=?MS.get(flow, f_uid), vizExp(flow, e), [], ())
+  | Value(v) => noop(~uid=f_uid, ~flow=?MS.get(flow, f_uid), vizValue(flow, v), [], ())
+  };
+
+let vizFrame = (flow, {uid, ctxts_uid, env_uid}) =>
+  vSeq(
+    ~uid,
+    ~flow=?MS.get(flow, uid),
+    [vizEnv(flow, env_uid), zipper(hole, vizCtxts(flow, ctxts_uid))],
+  );
+
+/* TODO: strips info */
+let rec stackToList = ((s_uid, s): stack_uid): list(frame) =>
+  switch (s) {
+  | Empty => []
+  | Stack(f, s) => [f, ...stackToList(s)]
+  };
+
+let vizStack = (flow, (s_uid, fs)) =>
+  switch (fs) {
+  | Empty => str(~uid=s_uid, ~flow=?MS.get(flow, s_uid), " ", ())
+  | _ =>
+    vSeq(
+      ~uid=s_uid,
+      ~flow=?MS.get(flow, s_uid),
+      stackToList((s_uid, fs)) |> List.map(vizFrame(flow)),
+    )
+  };
+
+let vizMachineState =
+    (
+      flow: FFS5Delta.flow,
+      {uid, zipper: {uid: z_uid, focus_uid, ctxts_uid}, env_uid, stack_uid},
+    ) => {
+  hSeq(
+    ~uid,
+    ~flow=?MS.get(flow, uid),
+    ~gap=20.,
+    [
+      vSeq(
+        ~gap=5.,
+        [
+          cell("env", vizEnv(flow, env_uid)),
+          zipper(
+            ~uid=z_uid,
+            ~flow=?MS.get(flow, z_uid),
+            vizFocus(flow, focus_uid),
+            vizCtxts(flow, ctxts_uid),
+          ),
+        ],
+      ),
+      cell("stack", vizStack(flow, stack_uid)),
+    ],
+  );
+};
